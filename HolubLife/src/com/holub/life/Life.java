ddac71f8a509ddb430ae6dfb6002fc5e11be63b8
seongmin cho <33325188+infelps@users.noreply.github.com>
2022-12-03 23:29:15 +0900

    refactor : delete DummyCell constructor

--- HolubLife/src/com/holub/life/Life.java
+++ HolubLife/src/com/holub/life/Life.java
@@ -15,36 +15,24 @@ import com.holub.ui.MenuSite;
  * @include /etc/license.txt
  */
 
-public final class Life extends JFrame
-{
-	public static void main( String[] arguments )
-	{	new Life();
-	}
+public final class Life extends JFrame {
+    public static void main(String[] arguments) {
+        new Life();
+    }
 
-	private Life()
-	{
-		//제목을 정해요 - 건들 필요 없고
-		super( "The Game of Life. "
-					+"(c)2003 Allen I. Holub <http://www.holub.com>");
+    private Life() {
+        super("The Game of Life. " + "(c)2003 Allen I. Holub <http://www.holub.com>");
 
-		//menusite로 시작을 한다 -> 수정해야 할 게 menusite라는 것을 알 수 있다.
+        MenuSite.establish(this);        //{=life.java.establish}
 
+        Clock clock = Clock.getInstance();
+        Universe universe = Universe.getInstance();
 
-		MenuSite.establish( this );		//{=life.java.establish}
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(UniverseView.getInstance(), BorderLayout.CENTER); //{=life.java.install}
 
-		Clock clock = Clock.getInstance();
-		DummyCell dummyCell = DummyCell.getInstance();
-		Universe universe = Universe.getInstance();
-		UniverseView universeView = UniverseView.getInstance();
-
-		setDefaultCloseOperation	( EXIT_ON_CLOSE 		);
-		getContentPane().setLayout	( new BorderLayout()	);
-		getContentPane().add( UniverseView.getInstance(), BorderLayout.CENTER); //{=life.java.install}
-
-		//pack()은 프레임내에 서브컴포넌트들의 레이아웃과 Prefered Size에
-		//맞도록 윈도우의 사이즈를 맞추는 작업이다. -> 딱히 건들 필요 없다
-		pack();
-		//visible도 건들 필요가 없다.
-		setVisible( true );
-	}
+        pack();
+        setVisible(true);
+    }
 }
